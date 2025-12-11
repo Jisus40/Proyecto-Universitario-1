5e1e42d1c6ee9c2291e73ead3c99fac0bc9850dc
Jisus40 <184026169+Jisus40@users.noreply.github.com>
2025-12-11 18:30:15 -0400

    commit 3
    Phase 1 of the project has been completed; however, the code needs improvement in several aspects. It currently works, but may contain several errors.

--- proyecto.cpp
+++ proyecto.cpp
@@ -4,25 +4,8 @@ using namespace std;
 
 int cantidadDeCaracteres;
 char caracterIngresado;
-
- /*bool esPrimo (int c){
-	        if (c==2){
-	            return true;
-	        }
-	        if(c%2==0){
-	            return false;
-	        }
-	        int limite = sqrt(c);
-	        
-	        for(int n=3; n<=limite; n+=2){
-	            if(c%n==0){
-	                return false;
-	            }
-	        }
-	           
- };*/ 
  
- void esPrimo(int numero){
+ bool esPrimo(int numero){
  	int contador = 0;
  	for (int i = 2; i < numero; i++)
  	{
@@ -36,8 +19,51 @@ char caracterIngresado;
  	if (contador > 0)
  	{
  		cout << "no es primo" << endl;
+ 		return false;
  	} else {
  		cout << "es primo" << endl;
+ 		return true;
+ 	}
+ }
+
+ int numeroPrimoMasCercano(int numero){
+ 	int posibilidad1 = numero + 1;
+ 	int posibilidad2 = numero - 1;
+ 	int primoPositivo = 0;
+ 	int primoNegativo = 0;
+
+ 	while(primoPositivo == 0){
+ 		bool result = esPrimo(posibilidad1);
+ 		if (result == false)
+ 		{
+ 			cout << "en este caso no fue primo: " << posibilidad1 << endl;
+ 			posibilidad1++;
+ 		} else {
+ 			cout << "en este caso si fue primo: " << posibilidad1 << endl;
+ 			primoPositivo += posibilidad1; 
+ 		}
+ 	}
+
+ 	while(primoNegativo == 0){
+ 		bool result = esPrimo(posibilidad2);
+ 		if (result == false)
+ 		{
+ 			cout << "en este caso no fue primo: " << posibilidad2 << endl;
+ 			posibilidad2--;
+ 		} else {
+ 			cout << "en este caso si fue primo: " << posibilidad2 << endl;
+ 			primoNegativo += posibilidad2; 
+ 		}
+ 	}
+
+ 	if ((primoPositivo - numero) < (numero - primoNegativo))
+ 	{
+ 		return primoPositivo;
+ 	} else if ((primoPositivo - numero) > (numero - primoNegativo))
+ 	{
+ 		return primoNegativo;
+ 	} else {
+ 		return primoPositivo;
  	}
  }
 
@@ -51,6 +77,30 @@ int main() {
 	    int caracterConvertido = caracterIngresado;
 	    cout << caracterConvertido << endl;
 	    esPrimo(caracterConvertido);
+	    int numeroPrimo = numeroPrimoMasCercano(caracterConvertido);
+	    cout << "el primo mas cercano es: " << numeroPrimo << endl;
+	    int d1;
+	    int d2;
+	    int d3;
+	    int digitoFinalFase1;
+	    if (numeroPrimo < 100)
+	    {
+	    	d1 = numeroPrimo / 10;
+	    	d2 = numeroPrimo % 10;
+	    	cout << "primer digito: " << d1 << endl;
+	    	cout << "segundo digito: " << d2 << endl;
+	    	digitoFinalFase1 = d1 + d2;
+	    	cout << "digito final de la fase 1: " << digitoFinalFase1 << endl;
+	    } else {
+	    	d1 = numeroPrimo / 100;
+	    	d2 = (numeroPrimo % 100) / 10;
+	    	d3 = numeroPrimo % 10;
+	    	cout << "primer digito: " << d1 << endl;
+	    	cout << "segundo digito: " << d2 << endl;
+	    	cout << "tercer digito: " << d3 << endl;
+	    	digitoFinalFase1 = d1 + d2 + d3;
+	    	cout << "digito final de la fase 1: " << digitoFinalFase1 << endl;
+	    }
 	    } 
 	    //cout<<esPrimo(caracterConvertido)<<endl;
 	system("pause");
